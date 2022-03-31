4fae93833d775d56fc47177f22e8594f2f144e85
Davis Gordon Dun <davis.dun@gmail.com>
2022-03-31 19:50:30 -0300

    initial files

---
+++ start-tedrosbox/src/main/java/com/tedros/Start.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.tedros;
+
+/**
+ * @author Davis Gordon
+ *
+ */
+public class Start extends Main {
+
+	public static void main(String[] args) { 
+    	launch(args); 
+    }
+}
