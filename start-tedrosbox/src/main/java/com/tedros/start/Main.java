fe9d910917dcd5870493d71efcfbf957f8abf67d
Davis Gordon Dun <davis.dun@gmail.com>
2022-04-11 20:21:03 -0300

    update

--- start-tedrosbox/src/main/java/com/tedros/start/Main.java
+++ start-tedrosbox/src/main/java/com/tedros/start/Main.java
@@ -1,13 +1,15 @@
 /**
  * 
  */
-package com.tedros;
+package com.tedros.start;
+
+import com.tedros.TedrosBox;
 
 /**
  * @author Davis Gordon
  *
  */
-public class Start extends Main {
+public class Main extends TedrosBox {
 
 	public static void main(String[] args) { 
     	launch(args); 
