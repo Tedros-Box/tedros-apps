72219e25a852549f6e8323214727ce2aaec032c4
Davis Gordon Dun <davis.dun@gmail.com>
2022-04-16 23:57:13 -0300

    added place, address and street type

---
+++ app-location/app-location-model/src/main/java/com/tedros/location/model/Address.java
@@ -0,0 +1,120 @@
+/**
+ * 
+ */
+package com.tedros.location.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.tedros.ejb.base.entity.TEntity;
+import com.tedros.location.domain.DomainSchema;
+import com.tedros.location.domain.DomainTables;
+
+/**
+ * @author Davis Gordon
+ *
+ */
+@Entity
+@Table(name = DomainTables.address, schema = DomainSchema.schema)
+public class Address extends TEntity {
+
+	private static final long serialVersionUID = 1L;
+
+	@ManyToOne
+	@JoinColumn(name="street_id", nullable=false)
+	private StreetType streetType;
+	
+	@Column(length=120, nullable=false)
+	private String plubicPlace;
+	
+	@Column(length=120, nullable=true)
+	private String complement;
+
+	@Column(length=120, nullable=true)
+	private String neighborhood;
+	
+	@ManyToOne
+	@JoinColumn(name="country_id", nullable=false)
+	private Country country;
+	
+	@ManyToOne
+	@JoinColumn(name="admarea_id", nullable=true)
+	private AdminArea adminArea;
+	
+	@ManyToOne
+	@JoinColumn(name="city_id", nullable=true)
+	private City city;
+	
+	@Column(length=20, nullable=true)
+	private String code;
+
+	public StreetType getStreetType() {
+		return streetType;
+	}
+
+	public void setStreetType(StreetType streetType) {
+		this.streetType = streetType;
+	}
+
+	public String getPlubicPlace() {
+		return plubicPlace;
+	}
+
+	public void setPlubicPlace(String plubicPlace) {
+		this.plubicPlace = plubicPlace;
+	}
+
+	public String getComplement() {
+		return complement;
+	}
+
+	public void setComplement(String complement) {
+		this.complement = complement;
+	}
+
+	public String getNeighborhood() {
+		return neighborhood;
+	}
+
+	public void setNeighborhood(String neighborhood) {
+		this.neighborhood = neighborhood;
+	}
+
+	public Country getCountry() {
+		return country;
+	}
+
+	public void setCountry(Country country) {
+		this.country = country;
+	}
+
+	public AdminArea getAdminArea() {
+		return adminArea;
+	}
+
+	public void setAdminArea(AdminArea adminArea) {
+		this.adminArea = adminArea;
+	}
+
+	public City getCity() {
+		return city;
+	}
+
+	public void setCity(City city) {
+		this.city = city;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+	
+	
+	
+}
---
+++ app-location/app-location-model/src/main/java/com/tedros/location/model/Place.java
@@ -0,0 +1,81 @@
+/**
+ * 
+ */
+package com.tedros.location.model;
+
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+import com.tedros.common.model.TFileEntity;
+import com.tedros.ejb.base.entity.TEntity;
+import com.tedros.location.domain.DomainSchema;
+import com.tedros.location.domain.DomainTables;
+
+/**
+ * @author Davis Gordon
+ *
+ */
+@Entity
+@Table(name = DomainTables.place, schema = DomainSchema.schema)
+public class Place extends TEntity {
+
+	private static final long serialVersionUID = 1L;
+
+	@Column(length=60, nullable=false)
+	private String title;
+	
+	@Column(nullable=true)
+	private String description;
+	
+	@ManyToOne
+	@JoinColumn(name="address_id", nullable=false)
+	private Address address;
+	
+	@ManyToMany
+	@JoinTable(name=DomainTables.place_picture, 
+	joinColumns=@JoinColumn(name="place_id"), 
+	inverseJoinColumns=@JoinColumn(name="file_id"),
+	uniqueConstraints=@UniqueConstraint(name="placePictureUK", 
+	columnNames = { "place_id","file_id"}))
+	public Set<TFileEntity> pictures;
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public Set<TFileEntity> getPictures() {
+		return pictures;
+	}
+
+	public void setPictures(Set<TFileEntity> pictures) {
+		this.pictures = pictures;
+	}
+}
