7b7c9f3729091f8e6c73ffffd4e53be46eaa9e7b
Davis Gordon Dun <davis.dun@gmail.com>
2022-05-17 22:47:28 -0300

    adding mapview (mapquest) on address

--- app-location/app-location-fx/src/main/java/com/tedros/location/module/address/model/AddressMV.java
+++ app-location/app-location-fx/src/main/java/com/tedros/location/module/address/model/AddressMV.java
@@ -10,6 +10,7 @@ import com.tedros.fxapi.annotation.control.TLabel;
 import com.tedros.fxapi.annotation.control.TOptionsList;
 import com.tedros.fxapi.annotation.control.TTextField;
 import com.tedros.fxapi.annotation.control.TTextInputControl;
+import com.tedros.fxapi.annotation.form.TSetting;
 import com.tedros.fxapi.annotation.layout.THBox;
 import com.tedros.fxapi.annotation.layout.THGrow;
 import com.tedros.fxapi.annotation.layout.TPane;
@@ -18,6 +19,9 @@ import com.tedros.fxapi.annotation.presenter.TEditModalPresenter;
 import com.tedros.fxapi.annotation.process.TEjbService;
 import com.tedros.fxapi.annotation.reader.TReaderHtml;
 import com.tedros.fxapi.annotation.scene.TNode;
+import com.tedros.fxapi.annotation.scene.control.TControl;
+import com.tedros.fxapi.annotation.scene.web.TWebEngine;
+import com.tedros.fxapi.annotation.scene.web.TWebView;
 import com.tedros.fxapi.presenter.model.TEntityModelView;
 import com.tedros.location.annotation.TAdminAreaComboBox;
 import com.tedros.location.annotation.TCityComboBox;
@@ -39,6 +43,7 @@ import javafx.scene.layout.Priority;
  *
  */
 @TEditModalPresenter()
+@TSetting(TAddressSetting.class)
 @TEjbService(serviceName = "IAddressControllerRemote", model=Address.class)
 @TSecurity(	id=DomainApp.ADDRESS_FORM_ID, 
 appName = "#{app.location.name}", moduleName = "#{module.administrative}", viewName = "#{view.address}",
@@ -50,44 +55,14 @@ public class AddressMV extends TEntityModelView<Address> {
 
 	private SimpleLongProperty id;
 	
+	/*@TTabPane(tabs = { @TTab(text = "#{label.main.data}", 
+			content = @TContent(detailForm=@TDetailForm(fields = {"country", "streetType", "complement"}))),
+			@TTab(text = "#{label.map}", 
+				content = @TContent(detailForm=@TDetailForm(fields = {"latitude", "webview"}))) 
+	})*/
 	private SimpleStringProperty display;
 	
-	@TReaderHtml
-	@TLabel(text="#{label.street.type}")
-	@TComboBoxField(firstItemTex="#{label.select}", required=true,
-		optionsList=@TOptionsList(serviceName = "IStreetTypeControllerRemote", 
-		optionModelViewClass=StreetTypeMV.class,
-		entityClass=StreetType.class))
-	@THBox(	pane=@TPane(children={"streetType", "plubicPlace"}), spacing=10, fillHeight=true,
-	hgrow=@THGrow(priority={@TPriority(field="streetType", priority=Priority.SOMETIMES), 
-			@TPriority(field="plubicPlace", priority=Priority.ALWAYS)}))
-	private SimpleObjectProperty<StreetType> streetType;
-	
-	@TReaderHtml
-	@TLabel(text="#{label.public.place}")
-	@TTextField(maxLength=120, required = true, 
-		node=@TNode(requestFocus=true, parse = true))
-	private SimpleStringProperty plubicPlace;
-	
-	@TReaderHtml
-	@TLabel(text="#{label.complement}")
-	@TTextField(maxLength=120, required = true)
-	@THBox(	pane=@TPane(children={"complement", "neighborhood", "code"}), spacing=10, fillHeight=true,
-	hgrow=@THGrow(priority={@TPriority(field="complement", priority=Priority.ALWAYS), 
-			@TPriority(field="neighborhood", priority=Priority.ALWAYS), 
-			@TPriority(field="code", priority=Priority.SOMETIMES)}))
-	private SimpleStringProperty complement;
 
-	@TReaderHtml
-	@TLabel(text="#{label.neighborhood}")
-	@TTextField(maxLength=120, required = false)
-	private SimpleStringProperty neighborhood;
-	
-	@TReaderHtml
-	@TLabel(text="#{label.address.code}")
-	@TTextField(maxLength=20, textInputControl=@TTextInputControl(promptText="#{text.address.code}", parse = true))
-	private SimpleStringProperty code;
-	
 	@TReaderHtml
 	@TLabel(text="#{label.country}")
 	@TCountryComboBox(firstItemTex="#{label.select}", required=true)
@@ -106,6 +81,68 @@ public class AddressMV extends TEntityModelView<Address> {
 	@TLabel(text="#{label.city}")
 	@TCityComboBox(firstItemTex="#{label.select}", countryField="country", adminAreaField="adminArea",  required=false)
 	private SimpleObjectProperty<City> city;
+	
+	
+	@TReaderHtml
+	@TLabel(text="#{label.street.type}")
+	@TComboBoxField(firstItemTex="#{label.select}", required=true,
+		optionsList=@TOptionsList(serviceName = "IStreetTypeControllerRemote", 
+		optionModelViewClass=StreetTypeMV.class,
+		entityClass=StreetType.class))
+	@THBox(	pane=@TPane(children={"streetType", "plubicPlace", "complement", "neighborhood", "code"}), spacing=10, fillHeight=true,
+	hgrow=@THGrow(priority={@TPriority(field="streetType", priority=Priority.SOMETIMES), 
+			@TPriority(field="plubicPlace", priority=Priority.ALWAYS),
+			@TPriority(field="complement", priority=Priority.ALWAYS), 
+			@TPriority(field="neighborhood", priority=Priority.ALWAYS), 
+			@TPriority(field="code", priority=Priority.SOMETIMES)}))
+	private SimpleObjectProperty<StreetType> streetType;
+	
+	@TReaderHtml
+	@TLabel(text="#{label.public.place}")
+	@TTextField(maxLength=120, required = true, 
+		control=@TControl(tooltip="#{text.mapview.press.enter}", parse = true), 
+		node=@TNode(requestFocus=true, parse = true))
+	private SimpleStringProperty plubicPlace;
+	
+	@TReaderHtml
+	@TLabel(text="#{label.complement}")
+	@TTextField(maxLength=120)
+	private SimpleStringProperty complement;
+
+	@TReaderHtml
+	@TLabel(text="#{label.neighborhood}")
+	@TTextField(maxLength=120)
+	private SimpleStringProperty neighborhood;
+	
+	@TReaderHtml
+	@TLabel(text="#{label.address.code}")
+	@TTextField(maxLength=20, 
+	control=@TControl(tooltip="#{text.mapview.press.enter}", parse = true),
+	textInputControl=@TTextInputControl(promptText="#{text.address.code}", parse = true))
+	private SimpleStringProperty code;
+	
+	@TReaderHtml
+	@TLabel(text="Latitude")
+	@TTextField(maxLength=15, 
+			control=@TControl(tooltip="#{text.mapview.press.enter}", parse = true))
+	@THBox(	pane=@TPane(children={"latitude", "logintude"}), spacing=10, fillHeight=true,
+	hgrow=@THGrow(priority={@TPriority(field="latitude", priority=Priority.ALWAYS), 
+			@TPriority(field="logintude", priority=Priority.ALWAYS)}))
+	private SimpleStringProperty latitude;
+	
+	@TReaderHtml
+	@TLabel(text="Logintude")
+	@TTextField(maxLength=15, 
+			control=@TControl(tooltip="#{text.mapview.press.enter}", parse = true))
+	private SimpleStringProperty logintude;
+	
+	/*@THyperlinkField(labeled = @TLabeled(text="View On Map", parse = true))
+	private SimpleStringProperty viewMap;*/
+	
+	@TWebView(prefHeight=300,
+			engine=@TWebEngine(//load="http://localhost:8081/editor-web-webapp/story/edit.html",
+			load="file:C:/Users/Davis Gordon/.tedros/MODULE/geolocation/location.html"))
+	private SimpleStringProperty webview;
 	
 	public AddressMV(Address entity) {
 		super(entity);
@@ -207,5 +244,45 @@ public class AddressMV extends TEntityModelView<Address> {
 	public void setDisplay(SimpleStringProperty display) {
 		this.display = display;
 	}
+
+
+	public SimpleStringProperty getLatitude() {
+		return latitude;
+	}
+
+
+	public void setLatitude(SimpleStringProperty latitude) {
+		this.latitude = latitude;
+	}
+
+
+	public SimpleStringProperty getLogintude() {
+		return logintude;
+	}
+
+
+	public void setLogintude(SimpleStringProperty logintude) {
+		this.logintude = logintude;
+	}
+
+
+	public SimpleStringProperty getWebview() {
+		return webview;
+	}
+
+
+	public void setWebview(SimpleStringProperty webview) {
+		this.webview = webview;
+	}
+
+/*
+	public SimpleStringProperty getViewMap() {
+		return viewMap;
+	}
+
+
+	public void setViewMap(SimpleStringProperty viewMap) {
+		this.viewMap = viewMap;
+	}*/
 
 }
