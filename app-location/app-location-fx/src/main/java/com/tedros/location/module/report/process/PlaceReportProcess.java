8b19fe63c8da6d25efb3f402b5143b9403f78ff1
Davis Gordon Dun <davis.dun@gmail.com>
2022-06-06 22:10:10 -0300

    improvements

--- app-location/app-location-fx/src/main/java/com/tedros/location/module/report/process/PlaceReportProcess.java
+++ app-location/app-location-fx/src/main/java/com/tedros/location/module/report/process/PlaceReportProcess.java
@@ -1,15 +1,9 @@
 package com.tedros.location.module.report.process;
 
-import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
-
-import org.apache.commons.lang3.StringUtils;
 
 import com.tedros.core.TLanguage;
 import com.tedros.fxapi.exception.TProcessException;
@@ -17,8 +11,6 @@ import com.tedros.fxapi.process.TReportProcess;
 import com.tedros.location.report.model.PlaceReportModel;
 import com.tedros.location.resource.AppResource;
 import com.tedros.location.start.TConstant;
-import com.tedros.settings.util.TOwnerProperties;
-import com.tedros.util.TedrosFolder;
 
 public class PlaceReportProcess extends TReportProcess<PlaceReportModel> {
 
@@ -27,30 +19,15 @@ public class PlaceReportProcess extends TReportProcess<PlaceReportModel> {
 	}
 	
 	protected HashMap<String, Object> getReportParameters() {
-		TOwnerProperties o = null;// new TOwnerProperties();
-		
 		TLanguage l = TLanguage.getInstance(TConstant.UUI);
-		InputStream logoIs = null;
-		if(o!=null && o.getLogotype()!=null && o.getLogotype().getByteEntity()!=null)
-			logoIs = new ByteArrayInputStream(o.getLogotype().getByteEntity().getBytes());
-		else
-			try {
-				logoIs = new FileInputStream(new File(TedrosFolder.IMAGES_FOLDER.getFullPath()+"logo-tedros-medium.png"));
-			} catch (FileNotFoundException e) {
-				e.printStackTrace();
-			}
+		super.setSubReportDir(AppResource.APP_MODULE_PATH);
 		HashMap<String, Object> params = new HashMap<String, Object>();
-		if(logoIs!=null)
-			params.put("logo", logoIs);
-		params.put("SUBREPORT_DIR", AppResource.APP_MODULE_PATH);
 		params.put("hTitle", l.getString("#{label.title}"));
 		params.put("hDesc", l.getString("#{label.description}"));
 		params.put("hType", l.getString("#{label.type}"));
 		params.put("hAddr", l.getString("#{label.address}"));
 		params.put("hContact", l.getString("#{label.contacts}"));
 		params.put("report_title", l.getString("#{header.jasper.place}"));
-		if(o!=null && StringUtils.isNotBlank(o.getOrganization()))
-			params.put("report_org", o.getOrganization());
 		return params;
 	}
 	
@@ -65,12 +42,7 @@ public class PlaceReportProcess extends TReportProcess<PlaceReportModel> {
 
 	@Override
 	protected void runAfterExport(Map<String, Object> params) {
-		if(params.containsKey("logo"))
-		try {
-			((InputStream)params.get("logo")).close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		
 	}
 
 }
