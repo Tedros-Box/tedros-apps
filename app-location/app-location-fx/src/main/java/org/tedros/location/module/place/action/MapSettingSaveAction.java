f0a6a68a9723c377fb62c1ac4b1b745956c6b845
Davis Gordon Dun <davis.dun@gmail.com>
2022-08-22 22:11:03 -0300

    message improvements

--- app-location/app-location-fx/src/main/java/org/tedros/location/module/place/action/MapSettingSaveAction.java
+++ app-location/app-location-fx/src/main/java/org/tedros/location/module/place/action/MapSettingSaveAction.java
@@ -6,19 +6,18 @@ package org.tedros.location.module.place.action;
 import java.util.Properties;
 
 import org.apache.commons.lang3.StringUtils;
+import org.tedros.core.TLanguage;
+import org.tedros.core.message.TMessage;
+import org.tedros.core.message.TMessageType;
+import org.tedros.fx.TFxKey;
+import org.tedros.fx.control.action.TPresenterAction;
+import org.tedros.fx.presenter.behavior.TActionType;
+import org.tedros.fx.presenter.dynamic.TDynaPresenter;
+import org.tedros.fx.presenter.entity.behavior.TSaveViewBehavior;
 import org.tedros.location.LocatKey;
 import org.tedros.location.module.place.model.MapSettingMV;
 import org.tedros.location.module.place.model.MapSettingModel;
 import org.tedros.location.resource.AppResource;
-
-import org.tedros.core.TLanguage;
-import org.tedros.fx.TFxKey;
-import org.tedros.fx.control.action.TPresenterAction;
-import org.tedros.fx.modal.TMessage;
-import org.tedros.fx.modal.TMessageType;
-import org.tedros.fx.presenter.behavior.TActionType;
-import org.tedros.fx.presenter.dynamic.TDynaPresenter;
-import org.tedros.fx.presenter.entity.behavior.TSaveViewBehavior;
 
 /**
  * @author Davis Gordon
