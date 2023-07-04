d9d68d496edd43ce63f63614dc5f04af1d08894b
Davis Gordon Dun <davis.dun@gmail.com>
2023-07-04 18:02:48 -0300

    improvements

--- app-samples/app-samples-fx/src/main/java/org/tedros/samples/module/price/PriceModule.java
+++ app-samples/app-samples-fx/src/main/java/org/tedros/samples/module/price/PriceModule.java
@@ -4,10 +4,12 @@
 package org.tedros.samples.module.price;
 
 import org.tedros.core.TModule;
+import org.tedros.core.annotation.TItem;
+import org.tedros.core.annotation.TView;
 import org.tedros.core.annotation.security.TAuthorizationType;
 import org.tedros.core.annotation.security.TSecurity;
-import org.tedros.fx.presenter.dynamic.view.TDynaView;
 import org.tedros.sample.domain.DomainApp;
+import org.tedros.sample.entity.ProductPrice;
 import org.tedros.samples.SmplsKey;
 import org.tedros.samples.module.price.model.PriceMV;
 
@@ -15,19 +17,13 @@ import org.tedros.samples.module.price.model.PriceMV;
  * @author Davis Gordon
  *
  */
+@TView(items = { 
+	@TItem(title = SmplsKey.VIEW_PRICE, 
+	model=ProductPrice.class, modelView=PriceMV.class) })
 @TSecurity(id=DomainApp.PRODUCT_PRICE_MODULE_ID, 
-appName = SmplsKey.APP_SAMPLES, 
-moduleName = SmplsKey.MODULE_PRICE, 
-allowedAccesses=TAuthorizationType.MODULE_ACCESS)
+	appName = SmplsKey.APP_SAMPLES, 
+	moduleName = SmplsKey.MODULE_PRICE, 
+	allowedAccesses=TAuthorizationType.MODULE_ACCESS)
 public class PriceModule extends TModule {
 	
-	/**
-	 * (non-Javadoc)
-	 * @see org.tedros.samples.core.ITModule#tStart()
-	 */
-	@Override
-	public void tStart() {
-		super.tShowView(new TDynaView<PriceMV>(this, PriceMV.class));
-	}
-
 }
