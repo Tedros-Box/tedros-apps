e4368e7988dc0cb989fe619fcae0bccfd43eb7cd
Davis <davis.dun@gmail.com>
2025-11-06 16:51:54 -0300

    tornado o startup assincrono

--- app-redmine-tools/app-redmine-tools-ejb/src/main/java/org/tedros/redminetools/server/ejb/service/RedmineStartService.java
+++ app-redmine-tools/app-redmine-tools-ejb/src/main/java/org/tedros/redminetools/server/ejb/service/RedmineStartService.java
@@ -1,48 +1,59 @@
 package org.tedros.redminetools.server.ejb.service;
 
-import javax.naming.NamingException;
-
 import org.tedros.core.setting.model.TPropertie;
 import org.tedros.core.support.TPropertieSupport;
 import org.tedros.redminetools.domain.RedminePropertie;
 import org.tedros.server.service.TServiceLocator;
 
 import jakarta.annotation.PostConstruct;
+import jakarta.annotation.Resource;
 import jakarta.ejb.Singleton;
 import jakarta.ejb.Startup;
 import jakarta.ejb.TransactionAttribute;
 import jakarta.ejb.TransactionAttributeType;
+import jakarta.enterprise.concurrent.ManagedExecutorService;
 
 @Startup
 @Singleton
 @TransactionAttribute(TransactionAttributeType.NOT_SUPPORTED)
 public class RedmineStartService {
-	
-	@PostConstruct
-	public void init() {
-		
-		TServiceLocator serv = TServiceLocator.getInstance();
-		
-		TPropertieSupport support;
-		try {
-			support = serv.lookup(TPropertieSupport.JNDI_NAME);
-		
-			for(RedminePropertie p : RedminePropertie.values()) { 			
-				TPropertie e = new TPropertie();
-				e.setName(p.name());
-				e.setKey(p.getValue());
-				e.setDescription(p.getDescription());
-				try {
-					support.create(e);
-				} catch (Exception e1) { 				
-					e1.printStackTrace();
-				}
-			}
-		} catch (NamingException e) {
-			e.printStackTrace();
-		} finally{
-			serv.close();
-		}
-	}
 
+    @Resource
+    private ManagedExecutorService executor; // pool de threads do TomEE
+
+    @PostConstruct
+    public void init() {
+        // Dispara assíncrono e SAI IMEDIATAMENTE (EAR continua inicializando)
+        executor.submit(()->executarInicializacaoAsync());
+    }
+    
+    private void executarInicializacaoAsync() {
+        try {
+            System.out.println("=== Iniciando carga de propriedades Redmine ASSÍNCRONA ===");
+            
+            TServiceLocator serv = TServiceLocator.getInstance();
+            try {
+                TPropertieSupport support = serv.lookupWithRetry(TPropertieSupport.JNDI_NAME);
+                
+                int contador = 0;
+                for (RedminePropertie p : RedminePropertie.values()) {
+                    TPropertie e = new TPropertie();
+                    e.setName(p.name());
+                    e.setKey(p.getValue());
+                    e.setDescription(p.getDescription());
+                    
+                    if (support.create(e)) {
+                        contador++;
+                    }
+                }
+                System.out.println("=== " + contador + "/" + RedminePropertie.values().length + " propriedades Redmine criadas com sucesso! ===");
+                
+            } finally {
+                serv.close();
+            }
+        } catch (Exception e) {
+            System.err.println("ERRO na inicialização Redmine: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
 }
